b3b88a1b1bbf106a1ffa10b6b633c03eda2fc750
joo98e <66362284+joo98e@users.noreply.github.com>
2022-08-08 23:11:24 +0900

    overview

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+/**
+ * @description 데코레이터
+ * nestjs는 데코레이터와 함께 하고, 클래스에 함수 기능을 추가할 수 있도록 한다.
+ * 클래스 위의 함수이며 클래스를 위해 움직인다.
+ */
 @Module({
   imports: [],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+/**
+ * @description main.ts의 이름은 변경할 수 없지만 함수 이름은 수정할 수 있다. (bootstrap => Start)
+ */
+async function Start() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+
+Start();
